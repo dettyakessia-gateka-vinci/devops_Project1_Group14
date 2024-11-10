f9bf5db0a1899e7be2e2b725e9d990c306ee6fec
Hadje <hadje.abakar@student.vinci.be>
2024-11-10 17:25:21 +0100

    refactor: extract the Main class instanciation

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,18 +1,15 @@
 import { Main } from "../src/app.js";
 
 describe("the function should return the answer when given a time", function() {
-  
-    it("buildLightMessageForSimpleMinutes should return OOOO when given 0 minute", function() {
-        let main = new Main();
+    let main = new Main();
 
+    it("buildLightMessageForSimpleMinutes should return OOOO when given 0 minute", function() {
         let result = main.buildLightMessageForSimpleMinutes(4, 0, "Y");
 
         expect(result).toBe("OOOO");
     });
 
     it("buildLightMessageForSimpleMinutes should return YOOO when given 1 minute", function() {
-        let main = new Main();
-
         let result = main.buildLightMessageForSimpleMinutes(4, 1, "Y");
 
         expect(result).toBe("YOOO");
