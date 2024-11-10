ff17cac84eca9946895df50e757c79f484e1d7e6
Hadje <hadje.abakar@student.vinci.be>
2024-11-10 17:28:22 +0100

    refactor: replace variables by constantes

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,21 +1,21 @@
 import { Main } from "../src/app.js";
 
 describe("the function should return the answer when given a time", function() {
-    let main = new Main();
+    const main = new Main();
 
     it("buildLightMessageForSimpleMinutes should return OOOO when given 0 minute", function() {
-        let result = main.buildLightMessageForSimpleMinutes(4, 0, "Y");
+        const result = main.buildLightMessageForSimpleMinutes(4, 0, "Y");
 
         expect(result).toBe("OOOO");
     });
 
     it("buildLightMessageForSimpleMinutes should return YOOO when given 1 minute", function() {
-        let result = main.buildLightMessageForSimpleMinutes(4, 1, "Y");
+        const result = main.buildLightMessageForSimpleMinutes(4, 1, "Y");
 
         expect(result).toBe("YOOO");
     });
     it("buildLightMessageForSimpleMinutes should return YYOO when given 2 minutes", function() {
-        let result = main.buildLightMessageForSimpleMinutes(4, 2, "Y");
+        const result = main.buildLightMessageForSimpleMinutes(4, 2, "Y");
 
         expect(result).toBe("YYOO");
     });
