e9d60354812474ca0dde3d30363aface8f2e60a7
awa-dieye-vinci <awa.dieye@student.vinci.be>
2024-11-10 17:21:32 +0100

    add: buildLightMessageForSimpleMinutes returns YOOO when given 1 minute

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -9,4 +9,12 @@ describe("the function should return the answer when given a time", function() {
 
         expect(result).toBe("OOOO");
     });
+
+    it("buildLightMessageForSimpleMinutes should return YOOO when given 1 minute", function() {
+        let main = new Main();
+
+        let result = main.buildLightMessageForSimpleMinutes(4, 1, "Y");
+
+        expect(result).toBe("YOOO");
+    });
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 export class Main {
     buildLightMessageForSimpleMinutes(totalAmountOfLights,amountOfTime, lightColor)
     {
+        if (amountOfTime === 1) return lightColor + "OOO";
+
         return "OOOO";
     }
 
